91ba81b8882a277613061dbf7cc810ac3ccdae4b
Gene Gotimer <eugene.gotimer@steampunk.com>
2020-11-28 11:13:18 -0500

    Removed unnecessary generic throws Exception

--- src/test/java/org/sonar/zaproxy/ui/ZapWidgetTest.java
+++ src/test/java/org/sonar/zaproxy/ui/ZapWidgetTest.java
@@ -29,7 +29,7 @@ import org.junit.jupiter.api.Test;
 public class ZapWidgetTest {
 
   @Test
-  public void railsTemplate() throws Exception {
+  public void railsTemplate() {
     ZapWidget widget = new ZapWidget();
     assertThat(widget.getClass().getResource(widget.getTemplatePath()))
         .as("Template not found: " + widget.getTemplatePath())
@@ -37,7 +37,7 @@ public class ZapWidgetTest {
   }
 
   @Test
-  public void metadata() throws Exception {
+  public void metadata() {
     ZapWidget widget = new ZapWidget();
     assertThat(widget.getId()).containsIgnoringCase("zap");
     assertThat(widget.getTitle()).contains("Vulnerabilities identified by ZAP");
